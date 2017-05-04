0336f62b6ed30c7cfdceb0e9b61f1e9aa66de7f8
zbishop3455 <zbishop3455@icloud.com>
2017-05-04 02:19:13 -0400

    Pieces drawn to board!
    Data structure for the board is complete. Now there is an option to
    select what side to play from. Now to add piece selection and moving.

--- Chess.js
+++ Chess.js
@@ -1,28 +1,80 @@
 
 
 //master demensions
-masterWidth = 1200;
-masterHeight = 800;
-pieces = [];
-
+masterSize = 800;
+letters = ['A','B','C','D','E','F','G','H'];
+whiteOrBlackGame = 0;
 
 function setup(){
-  createCanvas(masterWidth,masterHeight);
+  createCanvas(masterSize,masterSize);
   chessBoard = new board();
-  //make pieces
-  pieces.push(new King(0));
-  pieces.push(new King(1));
-  pieces.push(new Queen(0));
-  pieces.push(new Queen(1));
+  //make pieces and add them to the board
+
+  if(whiteOrBlackGame == 1){
+    chessBoard.chessData.set(0,"E",new King(0));
+    chessBoard.chessData.set(7,"E",new King(1));
+
+    chessBoard.chessData.set(0,"D",new Queen(0));
+    chessBoard.chessData.set(7,"D",new Queen(1));
+
+    chessBoard.chessData.set(0,"C",new Bishop(0));
+    chessBoard.chessData.set(0,"F",new Bishop(0));
+    chessBoard.chessData.set(7,"C",new Bishop(1));
+    chessBoard.chessData.set(7,"F",new Bishop(1));
+
+    chessBoard.chessData.set(0,"B",new Knight(0));
+    chessBoard.chessData.set(0,"G",new Knight(0));
+    chessBoard.chessData.set(7,"B",new Knight(1));
+    chessBoard.chessData.set(7,"G",new Knight(1));
+
+    chessBoard.chessData.set(0,"A",new Rook(0));
+    chessBoard.chessData.set(0,"H",new Rook(0));
+    chessBoard.chessData.set(7,"A",new Rook(1));
+    chessBoard.chessData.set(7,"H",new Rook(1));
+
+    for(var i=0;i<8;i++){
+      chessBoard.chessData.set(1,letters[i],new Pawn(0));
+      chessBoard.chessData.set(6,letters[i],new Pawn(1));
+    }
+  }else{
+    chessBoard.chessData.set(0,"E",new King(1));
+    chessBoard.chessData.set(7,"E",new King(0));
+
+    chessBoard.chessData.set(0,"D",new Queen(1));
+    chessBoard.chessData.set(7,"D",new Queen(0));
+
+    chessBoard.chessData.set(0,"C",new Bishop(1));
+    chessBoard.chessData.set(0,"F",new Bishop(1));
+    chessBoard.chessData.set(7,"C",new Bishop(0));
+    chessBoard.chessData.set(7,"F",new Bishop(0));
+
+    chessBoard.chessData.set(0,"B",new Knight(1));
+    chessBoard.chessData.set(0,"G",new Knight(1));
+    chessBoard.chessData.set(7,"B",new Knight(0));
+    chessBoard.chessData.set(7,"G",new Knight(0));
+
+    chessBoard.chessData.set(0,"A",new Rook(1));
+    chessBoard.chessData.set(0,"H",new Rook(1));
+    chessBoard.chessData.set(7,"A",new Rook(0));
+    chessBoard.chessData.set(7,"H",new Rook(0));
+
+    for(var i=0;i<8;i++){
+      chessBoard.chessData.set(1,letters[i],new Pawn(1));
+      chessBoard.chessData.set(6,letters[i],new Pawn(0));
+    }
+  }
+
 }
 
 function draw(){
-  background(40);
-
+  background(60);
   chessBoard.show();
+
 }
 
+function showPiece(piece){
 
+}
 
 board = function(){
   /*
@@ -32,32 +84,47 @@ board = function(){
     Data is stored in a p5 table
   */
 
-  this.size = 600;
-  this.gridSize = this.size / 8;
-  this.topOffset = 100;
-  this.sideOffset = 100;
-
   //make table
-  chessTable = new p5.Table();
-  letters = ['A','B','C','D','E','F','G','H'];
-
-  for(var i=0; i<letters.length; i++){
-    chessTable.addColumn(letters[i]);
-    chessTable.addRow();
+  this.chessData = new p5.Table();
+  //add col and new rows
+  for(var i=0;i<8;i++){
+    this.chessData.addRow();
+    this.chessData.addColumn(letters[i]);
   }
 
 
+  //draw grid
+  this.size = masterSize;
+  this.gridSize = this.size / 8;
+  this.topOffset = 0;
+  this.sideOffset = 0;
 
   this.show = function(){
-    //draw the table
-    for(var i=0;i<chessTable.getColumnCount();i++){
-
-      for(var j=0;j<chessTable.getRowCount();j++){
+    //draw the grid
+    for(var i=0;i<8;i++){
+      for(var j=0;j<8;j++){
         noFill();
         stroke(255);
         rect(this.gridSize*j + this.sideOffset,this.gridSize*i+ this.topOffset,this.gridSize,this.gridSize);
       }
     }
+
+    //draw all pieces
+
+    //loop through all pieces
+    for(var i=0;i<8;i++){
+      for(var j=0;j<8;j++){
+        //if there is a piece
+        if(this.chessData.get(i,j) != undefined){
+          //there is a piece, draw it
+          currentPiece = this.chessData.get(i,j);
+          img = currentPiece.picture;
+          image(img,j*this.gridSize,i*this.gridSize,100,100);
+
+        }
+      }
+    }
+
   }
 
 }
